a75aa6f82f1595748da99e6430b571d8ac5962ec
Stéphane Dauguet <sdauguet@gmail.com>
2022-12-05 15:57:58 +0100

    add simple pages and simple routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FloatingCart from "./Components/FloatingCart/FloatingCart";
+import NavBar from "./Components/Navbar/NavBar";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import ProductShowCase from "./Pages/ProductShowcase/ProductShowCase";
+import Contact from "./Pages/Contact/Contact";
+import ShoppingCart from "./Pages/ShopppingCart/ShoppingCart";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <NavBar />
+      <FloatingCart />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:id" component={ProductShowCase} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/shoppingcart" component={ShoppingCart} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Pages/ProductShowcase/ProductShowCase.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductShowCase = () => {
+  return <div>ProductShowCase</div>;
+};
+
+export default ProductShowCase;
